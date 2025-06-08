59dbbc9d2914ded5ba730e3c159279770b950cf3
kliuverson <kliverrui12@gmail.com>
2025-06-08 15:28:34 -0500

    le di mejor diseño a la vista loading

--- lib/features/home/presentation/views/loading_view.dart
+++ lib/features/home/presentation/views/loading_view.dart
@@ -1,20 +1,33 @@
 import 'package:flutter/material.dart';
 
 class Loading extends StatelessWidget {
-  const Loading({
-    super.key,
-  });
+  const Loading({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          CircularProgressIndicator(),
-          Text('Cargando...')
-        ],
+    return const Scaffold(
+      backgroundColor: Colors.black, // Fondo oscuro
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircularProgressIndicator(
+              color: Colors.deepPurpleAccent, // Color personalizado
+              strokeWidth: 4,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Cargando...',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.w400,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
