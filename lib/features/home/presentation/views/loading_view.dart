d32b9cd08d1bffa709ee1539eff55a6bbe2cbaf7
Jesus Bovea <jdavid.bovea@gmail.com>
2025-06-12 12:11:52 -0500

    Creacion de vista Loadiong

--- lib/features/home/presentation/views/loading_view.dart
+++ lib/features/home/presentation/views/loading_view.dart
@@ -1,31 +1,47 @@
 import 'package:flutter/material.dart';
 
-class Loading extends StatelessWidget {
-  const Loading({super.key});
+class LoadingView extends StatelessWidget {
+  final String message;
+
+  const LoadingView({super.key, this.message = "Cargando, por favor espera..."});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black, // Fondo oscuro
-      body: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            CircularProgressIndicator(
-              color: Colors.deepPurpleAccent, // Color personalizado
-              strokeWidth: 4,
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Cargando...',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 18,
-                fontWeight: FontWeight.w400,
-                letterSpacing: 1.2,
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blueAccent, Colors.lightBlueAccent],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const Icon(
+                Icons.cloud_download,
+                size: 80,
+                color: Colors.white70,
               ),
-            ),
-          ],
+              const SizedBox(height: 30),
+              const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                strokeWidth: 4,
+              ),
+              const SizedBox(height: 20),
+              Text(
+                message,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
